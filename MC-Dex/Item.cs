e148d7fbf70bed15e3e80ed737ff0fa60785131f
Eloan ANDRÉ <Eloan.ANDRE@etu.uca.fr>
2022-05-04 17:07:22 +0200

    classe

---
+++ MC-Dex/Item.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MC_Dex
+{
+    internal class Item
+    {
+        public string Nom
+        {
+            get 
+            { 
+                return nom; 
+            }
+            set
+            {
+                nom = value; 
+            }    
+        }
+        private string nom;
+
+        public string Id
+        {
+            get
+            {
+                return identifiant;
+            }
+            set
+            {
+                identifiant = value;
+            }
+        }
+        private string identifiant;
+
+        public string Image
+        {
+            get
+            {
+                return image;
+            }
+            set
+            {
+                image = value;
+            }
+        }
+        private string image;
+
+        public string Desc
+        {
+            get
+            {
+                return description;
+            }
+            set
+            {
+                description = value;
+            }
+        }
+        private string description;
+
+        public string NomE
+        {
+            get
+            {
+                return nomAnglais;
+            }
+            set
+            {
+                nomAnglais = value;
+            }
+        }
+        private string nomAnglais = default;
+
+
+        private IDictionary<string, float> listeStats = new Dictionary<string, float>();
+        public float this[string key]
+        {
+           get 
+            {
+                return listeStats[key];
+             }   
+           set
+            {
+                listeStats[key] = value;
+            }
+        }
+    }
+
+
+
+}
---
+++ MC-Dex/titouan/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MC_Dex
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void Button_home(object sender, RoutedEventArgs e)
+        {
+            contentControl.Content = new home();
+        }
+
+        public void Button_ajouter(object sender, RoutedEventArgs e)
+        {
+            contentControl.Content = new pageAjouter();
+        }
+
+        public void Button_rechercher(object sender, RoutedEventArgs e)
+        {
+            contentControl.Content = new pageRechercher();
+        }
+
+    }
+}
